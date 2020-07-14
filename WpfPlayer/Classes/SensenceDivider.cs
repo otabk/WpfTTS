6896c81fc9739a230105e490f78c8e9c8796227a
Otabek Qayumov <tm_gambit@live.ru>
2020-07-14 19:22:41 +0500

    addes sentence divider

---
+++ WpfPlayer/Classes/SensenceDivider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace WpfPlayer.Classes
+{
+	class SensenceDivider
+	{
+		Regex regex = new Regex(@"[^.!?]*[.!?]");
+		private string _text;
+		public SensenceDivider(string text)
+		{
+			_text = text;
+		}
+
+		public List<string> GetSentences()
+		{
+			List<string> list = null;
+			if (!string.IsNullOrEmpty(_text))
+			{
+				list = new List<string>();
+				var matchs = regex.Matches(_text);
+				if (matchs.Count > 0)
+					foreach (Match item in matchs)
+					{
+						list.Add(item.Value.Trim());
+					}
+			}
+			return list;
+		}
+	}
+}
