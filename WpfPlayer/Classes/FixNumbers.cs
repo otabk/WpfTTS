85e2235aafa8adb7f47705c71789ca0932c3536a
Otabek Qayumov <tm_gambit@live.ru>
2020-07-22 19:11:47 +0500

    133131

--- WpfPlayer/Classes/FixNumbers.cs
+++ WpfPlayer/Classes/FixNumbers.cs
@@ -4,18 +4,20 @@ namespace WpfPlayer.Classes
 {
 	class FixNumbers
 	{
-		Regex fractional = new Regex(@"[0-9]+[,.][0-9]+");
+		//Regex fractional = new Regex(@"[0-9]+[,.][0-9]+");
+		string _pattern = @"[0-9]+[,.][0-9]+";
 		public FixNumbers()
 		{
 		}
 
 		public string Fix (string text)
 		{
-			var fmatch = fractional.Matches(text);
-			if (fmatch.Count > 0)
-			{
-				fractional.Replace("", );
-			}
+			//var fmatch = fractional.Matches(text);
+			//if (fmatch.Count > 0)
+			//{
+			//	fractional.Replace("", );
+			//}
+			var reslttext = Regex.Replace(text, _pattern, "", RegexOptions.IgnoreCase);
 			return text;
 		}
 	}
